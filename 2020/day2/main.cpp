0ea09ac65495f80e47a3fa64047b34053b0d5f87
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2020-12-02 18:11:19 -0500

    update

---
+++ 2020/day2/main.cpp
@@ -0,0 +1,8 @@
+#include "part1.h"
+
+int main(int argc, char const *argv[])
+{
+   part1::main(argc, argv);
+   return 0;
+}
+
---
+++ 2020/day2/part1.h
@@ -0,0 +1,12 @@
+#if !defined(PART1_H)
+#define PART1_H
+#include <iostream>
+
+namespace part1
+{
+int main(int argc, char const *argv[]) {
+   return 0;
+}
+}  // namespace part1
+
+#endif  // PART1_H
