c1f3357562186c1928d527eed616fec65467fbee
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2022-02-10 10:19:20 -0500

    day 11

--- 2021/aofc_2021/src/days/day11.rs
+++ 2021/aofc_2021/src/days/day11.rs
@@ -73,7 +73,57 @@ pub fn day11_1() -> impl std::fmt::Debug {
 }
 
 pub fn day11_2() -> impl std::fmt::Debug {
-    let input_file = format!("{}/aofc_2021/input/day11", env!("ADVENT_OF_CODE_2021"));
+    // let input_file = format!("{}/aofc_2021/input/day11", env!("ADVENT_OF_CODE_2021"));
+    let input_file = format!("{}/aofc_2021/input/test", env!("ADVENT_OF_CODE_2021"));
     let _data = fs::read_to_string(input_file);
-    todo!()
+    let mut point_map = PointMap::default();
+    for (y, line) in _data.unwrap().split("\n").enumerate() {
+        for (x, char) in line.chars().enumerate() {
+            point_map[(x, y)] = char.to_digit(10).unwrap();
+        }
+    }
+    let dimentions = point_map.get_dimentions();
+    let map_len = dimentions.0 * dimentions.1;
+    let flashes = Rc::new(RefCell::new(0));
+    let already_exploded = Rc::new(RefCell::new(vec![]));
+    let mut count = 0;
+    while {let b = already_exploded.borrow(); b.len() < map_len} {
+        {
+            let mut b = already_exploded.borrow_mut();
+            b.clear();
+        }
+        count += 1;
+
+        for point in DimentionIter::new(dimentions) {
+            point_map[point] += 1;
+        }
+        let mut trip = true;
+        while trip {
+            trip = false;
+            for point in DimentionIter::new(dimentions) {
+                if point_map[point] > 9 {
+                    {
+                        let b = already_exploded.borrow();
+                        if b.contains(&point) {
+                            continue;
+                        }
+                    }
+                    trip = true;
+                    handle_explosion(
+                        &mut point_map,
+                        point,
+                        already_exploded.clone(),
+                        flashes.clone(),
+                    );
+                }
+            }
+        }
+        for point in DimentionIter::new(dimentions) {
+            if point_map[point] > 9 {
+                point_map[point] = 0
+            }
+        }
+    }
+
+    count
 }
--- 2021/aofc_2021/src/main.rs
+++ 2021/aofc_2021/src/main.rs
@@ -1,5 +1,5 @@
 use aofc_2021::prelude::*;
 fn main() {
     println!("part 1: {:?}", day11_1());
-    // println!("part 2: {:?}", day11_2());
+    println!("part 2: {:?}", day11_2());
 }
