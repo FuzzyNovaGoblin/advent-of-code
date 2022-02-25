08079c0676efd37cd56d0237af158b65d2ab59d1
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2022-02-25 17:58:37 -0500

    day 18

--- 2021/aofc_2021/src/days/day18.rs
+++ 2021/aofc_2021/src/days/day18.rs
@@ -1,32 +1,313 @@
-use std::fs;
+#![allow(dead_code, unused)]
+use std::{
+    fs,
+    ops::{Add, AddAssign},
+};
+
+use regex::Regex;
+
+use crate::AnsType;
+
+// enum SnailfishVal {
+//     Val(u32),
+//     SNum(Box<SnailfishNum>),
+// }
+
+// struct SnailfishNum{
+//     x: SnailfishVal,
+//     y: SnailfishVal
+// }
+
+#[derive(Debug, Clone, Copy)]
+enum SnailfishSymbol {
+    OpenBraket,
+    ClosedBraket,
+    Val(i32),
+}
+
+impl From<&str> for SnailfishSymbol {
+    fn from(from_str: &str) -> Self {
+        match from_str {
+            "[" => SnailfishSymbol::OpenBraket,
+            "]" => SnailfishSymbol::ClosedBraket,
+            s => SnailfishSymbol::Val(
+                s.parse()
+                    .expect("if not '[' or ']' symbol should parse to `i32`"),
+            ),
+        }
+    }
+}
+
+impl AddAssign<i32> for SnailfishSymbol {
+    fn add_assign(&mut self, rhs: i32) {
+        if let SnailfishSymbol::Val(v) = self {
+            *v += rhs;
+        } else {
+            panic!()
+        }
+    }
+}
+impl AddAssign for SnailfishSymbol {
+    fn add_assign(&mut self, rhs: SnailfishSymbol) {
+        if let (SnailfishSymbol::Val(s_val), SnailfishSymbol::Val(other)) = (self, rhs) {
+            *s_val += other;
+        } else {
+            panic!()
+        }
+    }
+}
+impl Add for SnailfishSymbol {
+    type Output = SnailfishSymbol;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        if let (SnailfishSymbol::Val(s_val), SnailfishSymbol::Val(other)) = (self, rhs) {
+            SnailfishSymbol::Val(s_val + other)
+        } else {
+            panic!()
+        }
+    }
+}
+impl Add<i32> for SnailfishSymbol {
+    type Output = SnailfishSymbol;
+
+    fn add(self, rhs: i32) -> Self::Output {
+        match self {
+            SnailfishSymbol::Val(v) => SnailfishSymbol::Val(v + rhs),
+            _ => panic!(),
+        }
+    }
+}
+
+#[derive(Clone)]
+struct SnailfishNum {
+    data: Vec<SnailfishSymbol>,
+}
+
+impl std::fmt::Debug for SnailfishNum {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "SnailfishNum: {}", self)
+    }
+}
+impl std::fmt::Display for SnailfishSymbol {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                SnailfishSymbol::OpenBraket => "[".to_owned(),
+                SnailfishSymbol::ClosedBraket => "]".to_owned(),
+                SnailfishSymbol::Val(v) => v.to_string(),
+            }
+        )
+    }
+}
+
+impl Add for SnailfishNum {
+    type Output = SnailfishNum;
+
+    fn add(mut self, mut rhs: Self) -> Self::Output {
+        let mut ret_val = SnailfishNum::new(vec![SnailfishSymbol::OpenBraket]);
+        ret_val.data.append(&mut self.data);
+        ret_val.data.append(&mut rhs.data);
+        ret_val.data.push(SnailfishSymbol::ClosedBraket);
+
+        ret_val.reduce();
+
+        ret_val
+    }
+}
+
+impl std::fmt::Display for SnailfishNum {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut itr = self.data.iter().peekable();
+        while let Some(sym) = itr.next() {
+            write!(f, "{}", sym)?;
+            match (sym, itr.peek()) {
+                (SnailfishSymbol::ClosedBraket, Some(SnailfishSymbol::ClosedBraket)) => {}
+                (
+                    SnailfishSymbol::Val(_) | SnailfishSymbol::ClosedBraket,
+                    Some(SnailfishSymbol::Val(_) | SnailfishSymbol::OpenBraket),
+                ) => write!(f, ",")?,
+                _ => (),
+            }
+        }
+        Ok(())
+    }
+}
+
+impl SnailfishNum {
+    fn calc_magnitude(&self) -> i32 {
+        let mut data = self.data.clone();
+        'mag: loop {
+            for i in 0..(data.len() - 1) {
+                if let (SnailfishSymbol::Val(left), SnailfishSymbol::Val(right)) =
+                    (data[i], data[i + 1])
+                {
+                    for _ in 0..4 {
+                        data.remove(i - 1);
+                    }
+                    data.insert(i - 1, SnailfishSymbol::Val(3 * left + 2 * right));
+                    continue 'mag;
+                }
+            }
+            break;
+        }
+        match data[0] {
+            SnailfishSymbol::Val(v) => v,
+            _ => unimplemented!(),
+        }
+    }
+    fn new(data: Vec<SnailfishSymbol>) -> Self {
+        let mut val = Self { data };
+        val.reduce();
+        val
+    }
+
+    pub fn get_next_lit(&self, index: usize) -> Option<usize> {
+        for i in index..self.data.len() {
+            if let SnailfishSymbol::Val(_) = self.data[i] {
+                return Some(i);
+            }
+        }
+        None
+    }
+
+    pub fn reduce(&mut self) {
+        'reduce: loop {
+            let mut depth = 0;
+            let mut last_num = None;
+
+            for i in 0..self.data.len() {
+                let curent_value = match self.data[i] {
+                    SnailfishSymbol::OpenBraket => {
+                        depth += 1;
+                        continue;
+                    }
+                    SnailfishSymbol::ClosedBraket => {
+                        depth -= 1;
+                        continue;
+                    }
+                    SnailfishSymbol::Val(v) => v,
+                };
+                if let (Some(SnailfishSymbol::OpenBraket), Some(SnailfishSymbol::ClosedBraket)) =
+                    (self.data.get(i), self.data.get(i + 1))
+                {
+                    self.data.remove(i);
+                    self.data.remove(i);
+                    continue 'reduce;
+                } else if depth > 4 {
+                    if let Some(SnailfishSymbol::Val(_)) = self.data.get(i + 1) {
+                        if let Some(last) = last_num {
+                            self.data[last] += curent_value
+                        }
+                        if let Some(index) = self.get_next_lit(i + 2) {
+                            let num = self.data.remove(i + 1);
+                            self.data[index - 1] += num;
+                        } else {
+                            self.data.pop();
+                        }
+                        for _ in 0..3 {
+                            self.data.remove(i - 1);
+                        }
+                        self.data.insert(i - 1, SnailfishSymbol::Val(0));
+                        // dbg!("continue");
+                        continue 'reduce;
+                    }
+                    panic!();
+                    // dbg!("continue");
+                    continue 'reduce;
+                } else {
+                    last_num = Some(i)
+                }
+            }
+            for i in 0..self.data.len() {
+                let curent_value = match self.data[i] {
+                    SnailfishSymbol::OpenBraket => {
+                        depth += 1;
+                        continue;
+                    }
+                    SnailfishSymbol::ClosedBraket => {
+                        depth -= 1;
+                        continue;
+                    }
+                    SnailfishSymbol::Val(v) => v,
+                };
+                if curent_value >= 10 {
+                    self.data.remove(i);
+                    self.data.insert(i, SnailfishSymbol::ClosedBraket);
+                    self.data.insert(
+                        i,
+                        SnailfishSymbol::Val(if curent_value % 2 == 0 {
+                            curent_value / 2
+                        } else {
+                            curent_value / 2 + 1
+                        }),
+                    );
+                    self.data.insert(i, SnailfishSymbol::Val(curent_value / 2));
+                    self.data.insert(i, SnailfishSymbol::OpenBraket);
+                    // dbg!("continue");
+                    continue 'reduce;
+                }
+            }
+            break;
+        }
+    }
+
+    pub fn reduce_nested_pair(&mut self, depth: usize) {}
+}
 
 pub fn day18_1(file_name: &str) -> impl crate::AnsType {
-let input_file = format!("{}/aofc_2021/input/{}", env!("ADVENT_OF_CODE_2021"), file_name);
-    let _data = fs::read_to_string(input_file);
-	todo!()
+    let input_file = format!(
+        "{}/aofc_2021/input/{}",
+        env!("ADVENT_OF_CODE_2021"),
+        file_name
+    );
+    let reg_expr = Regex::new(r"((?:\d+)|(?:\[)|(?:\]))").unwrap();
+    let file_data = fs::read_to_string(input_file).unwrap();
+    let mut data = file_data
+        .split('\n')
+        .map(|line| {
+            SnailfishNum::new(
+                reg_expr
+                    .captures_iter(line)
+                    .map(|cap| SnailfishSymbol::from(cap.get(1).unwrap().as_str()))
+                    .collect(),
+            )
+        })
+        .reduce(|f_val, new_val| (f_val + new_val));
+
+    data.unwrap().calc_magnitude()
 }
 
 pub fn day18_2(file_name: &str) -> impl crate::AnsType {
-let input_file = format!("{}/aofc_2021/input/{}", env!("ADVENT_OF_CODE_2021"), file_name);
-    let _data = fs::read_to_string(input_file);
-	todo!()
-}
+    let input_file = format!(
+        "{}/aofc_2021/input/{}",
+        env!("ADVENT_OF_CODE_2021"),
+        file_name
+    );
+    let reg_expr = Regex::new(r"((?:\d+)|(?:\[)|(?:\]))").unwrap();
+    let file_data = fs::read_to_string(input_file).unwrap();
+    let mut data = file_data
+        .split('\n')
+        .map(|line| {
+            SnailfishNum::new(
+                reg_expr
+                    .captures_iter(line)
+                    .map(|cap| SnailfishSymbol::from(cap.get(1).unwrap().as_str()))
+                    .collect(),
+            )
+        })
+        .collect::<Vec<_>>();
 
-
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use crate::assert_eq_ansval;
-
-    #[test]
-    #[ignore]
-    fn t1() {
-        assert_eq_ansval!((), day18_1("test"));
+    let mut max_mag = 0;
+    for a in 0..data.len() {
+        for b in 0..data.len() {
+            let sum = data[a].clone() + data[b].clone();
+            let mag = sum.calc_magnitude();
+            if mag > max_mag {
+                max_mag = mag;
+            }
+        }
     }
-    #[test]
-    #[ignore]
-    fn t2() {
-        assert_eq_ansval!((), day18_2("test"));
-    }
+    max_mag
 }
--- 2021/aofc_2021/src/lib.rs
+++ 2021/aofc_2021/src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(default_free_fn, hash_drain_filter, auto_traits, negative_impls)]
 
-use std::fmt::Display;
+use std::fmt::{Debug, Display};
 pub mod days {
     pub mod day1;
     pub mod day10;
@@ -87,6 +87,7 @@ pub trait AnsType {
 
 auto trait AnsNotDisplay {}
 impl !AnsNotDisplay for () {}
+impl<T: Debug> !AnsNotDisplay for Option<T> {}
 
 impl AnsType for () {
     fn value(&self) -> String {
@@ -94,64 +95,230 @@ impl AnsType for () {
     }
 }
 
+impl<T: Debug> AnsType for Option<T> {
+    fn value(&self) -> String {
+        match self {
+            Some(v) => format!("{:?}", v),
+            None => String::from("None"),
+        }
+    }
+}
+
+// auto trait NotDisplay {}
+// impl<T:Display> !NotDisplay for T {}
+// impl<T:AnsNotDisplay> !NotDisplay for T {}
+
+
+
 impl<T: Display + AnsNotDisplay> AnsType for T {
     fn value(&self) -> String {
         format!("{}", self)
     }
 }
 
-
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::days;
-    #[test]fn day1_1(){assert_eq_ansval!(1602, days::day1::day1_1("day1") );}
-    #[test]fn day1_2(){assert_eq_ansval!(1633, days::day1::day1_2("day1") );}
-    #[test]fn day2_1(){assert_eq_ansval!(2091984, days::day2::day2_1("day2") );}
-    #[test]fn day2_2(){assert_eq_ansval!(2086261056, days::day2::day2_2("day2") );}
-    #[test]fn day3_1(){assert_eq_ansval!(3882564, days::day3::day3_1("day3") );}
-    #[test]fn day3_2(){assert_eq_ansval!(3385170, days::day3::day3_2("day3") );}
-    #[test]fn day4_1(){assert_eq_ansval!(39984, days::day4::day4_1("day4") );}
-    #[test]fn day4_2(){assert_eq_ansval!(8468, days::day4::day4_2("day4") );}
-    #[test]fn day5_1(){assert_eq_ansval!(6856, days::day5::day5_1("day5") );}
-    #[test]fn day5_2(){assert_eq_ansval!(20666, days::day5::day5_2("day5") );}
-    #[test]fn day6_1(){assert_eq_ansval!(343441, days::day6::day6_1("day6") );}
-    #[test]fn day6_2(){assert_eq_ansval!(1569108373832_usize, days::day6::day6_2("day6") );}
-    #[test]fn day7_1(){assert_eq_ansval!(328262, days::day7::day7_1("day7") );}
-    #[test]fn day7_2(){assert_eq_ansval!(90040997, days::day7::day7_2("day7") );}
-    #[test]fn day8_1(){assert_eq_ansval!(390, days::day8::day8_1("day8") );}
-    #[test]fn day8_2(){assert_eq_ansval!(1011785, days::day8::day8_2("day8") );}
-    #[test]fn day9_1(){assert_eq_ansval!(564, days::day9::day9_1("day9") );}
-    #[test]fn day9_2(){assert_eq_ansval!(1038240, days::day9::day9_2("day9") );}
-    #[test]fn day10_1(){assert_eq_ansval!(339411, days::day10::day10_1("day10") );}
-    #[test]fn day10_2(){assert_eq_ansval!(2289754624_usize, days::day10::day10_2("day10") );}
-    #[test]fn day11_1(){assert_eq_ansval!(1741, days::day11::day11_1("day11") );}
-    #[test]fn day11_2(){assert_eq_ansval!(440, days::day11::day11_2("day11") );}
-    #[test]fn day12_1(){assert_eq_ansval!(4304, days::day12::day12_1("day12") );}
-    #[test]fn day12_2(){assert_eq_ansval!(118242, days::day12::day12_2("day12") );}
-    #[test]fn day13_1(){assert_eq_ansval!(607, days::day13::day13_1("day13") );}
-    #[test]fn day13_2(){assert_eq_ansval!(".##..###..####.#....###..####.####.#....\n#..#.#..#....#.#....#..#.#.......#.#....\n#....#..#...#..#....#..#.###....#..#....\n#....###...#...#....###..#.....#...#....\n#..#.#....#....#....#....#....#....#....\n.##..#....####.####.#....#....####.####.\n", days::day13::day13_2("day13") );}
-    #[test]fn day14_1(){assert_eq_ansval!(3259, days::day14::day14_1("day14") );}
-    #[test]fn day14_2(){assert_eq_ansval!(3459174981021_usize, days::day14::day14_2("day14") );}
-    #[test]fn day15_1(){assert_eq_ansval!(748, days::day15::day15_1("day15") );}
-    #[test]fn day15_2(){assert_eq_ansval!(3045, days::day15::day15_2("day15") );}
-    #[test]fn day16_1(){assert_eq_ansval!(1012, days::day16::day16_1("day16") );}
-    #[test]fn day16_2(){assert_eq_ansval!(2223947372407_usize, days::day16::day16_2("day16") );}
-    #[test]fn day17_1(){assert_eq_ansval!(4656, days::day17::day17_1("day17") );}
-    #[test]fn day17_2(){assert_eq_ansval!(1908, days::day17::day17_2("day17") );}
-    #[test] #[ignore]fn day18_1(){assert_eq_ansval!((), days::day18::day18_1("day18") );}
-    #[test] #[ignore]fn day18_2(){assert_eq_ansval!((), days::day18::day18_2("day18") );}
-    #[test] #[ignore]fn day19_1(){assert_eq_ansval!((), days::day19::day19_1("day19") );}
-    #[test] #[ignore]fn day19_2(){assert_eq_ansval!((), days::day19::day19_2("day19") );}
-    #[test] #[ignore]fn day20_1(){assert_eq_ansval!((), days::day20::day20_1("day20") );}
-    #[test] #[ignore]fn day20_2(){assert_eq_ansval!((), days::day20::day20_2("day20") );}
-    #[test] #[ignore]fn day21_1(){assert_eq_ansval!((), days::day21::day21_1("day21") );}
-    #[test] #[ignore]fn day21_2(){assert_eq_ansval!((), days::day21::day21_2("day21") );}
-    #[test] #[ignore]fn day22_1(){assert_eq_ansval!((), days::day22::day22_1("day22") );}
-    #[test] #[ignore]fn day22_2(){assert_eq_ansval!((), days::day22::day22_2("day22") );}
-    #[test] #[ignore]fn day23_1(){assert_eq_ansval!((), days::day23::day23_1("day23") );}
-    #[test] #[ignore]fn day23_2(){assert_eq_ansval!((), days::day23::day23_2("day23") );}
-    #[test] #[ignore]fn day24_1(){assert_eq_ansval!((), days::day24::day24_1("day24") );}
-    #[test] #[ignore]fn day24_2(){assert_eq_ansval!((), days::day24::day24_2("day24") );}
-    #[test] #[ignore]fn day25_1(){assert_eq_ansval!((), days::day25::day25_1("day25") );}
-    #[test] #[ignore]fn day25_2(){assert_eq_ansval!((), days::day25::day25_2("day25") );}
+    #[test]
+    fn day1_1() {
+        assert_eq_ansval!(1602, days::day1::day1_1("day1"));
+    }
+    #[test]
+    fn day1_2() {
+        assert_eq_ansval!(1633, days::day1::day1_2("day1"));
+    }
+    #[test]
+    fn day2_1() {
+        assert_eq_ansval!(2091984, days::day2::day2_1("day2"));
+    }
+    #[test]
+    fn day2_2() {
+        assert_eq_ansval!(2086261056, days::day2::day2_2("day2"));
+    }
+    #[test]
+    fn day3_1() {
+        assert_eq_ansval!(3882564, days::day3::day3_1("day3"));
+    }
+    #[test]
+    fn day3_2() {
+        assert_eq_ansval!(3385170, days::day3::day3_2("day3"));
+    }
+    #[test]
+    fn day4_1() {
+        assert_eq_ansval!(39984, days::day4::day4_1("day4"));
+    }
+    #[test]
+    fn day4_2() {
+        assert_eq_ansval!(8468, days::day4::day4_2("day4"));
+    }
+    #[test]
+    fn day5_1() {
+        assert_eq_ansval!(6856, days::day5::day5_1("day5"));
+    }
+    #[test]
+    fn day5_2() {
+        assert_eq_ansval!(20666, days::day5::day5_2("day5"));
+    }
+    #[test]
+    fn day6_1() {
+        assert_eq_ansval!(343441, days::day6::day6_1("day6"));
+    }
+    #[test]
+    fn day6_2() {
+        assert_eq_ansval!(1569108373832_usize, days::day6::day6_2("day6"));
+    }
+    #[test]
+    fn day7_1() {
+        assert_eq_ansval!(328262, days::day7::day7_1("day7"));
+    }
+    #[test]
+    fn day7_2() {
+        assert_eq_ansval!(90040997, days::day7::day7_2("day7"));
+    }
+    #[test]
+    fn day8_1() {
+        assert_eq_ansval!(390, days::day8::day8_1("day8"));
+    }
+    #[test]
+    fn day8_2() {
+        assert_eq_ansval!(1011785, days::day8::day8_2("day8"));
+    }
+    #[test]
+    fn day9_1() {
+        assert_eq_ansval!(564, days::day9::day9_1("day9"));
+    }
+    #[test]
+    fn day9_2() {
+        assert_eq_ansval!(1038240, days::day9::day9_2("day9"));
+    }
+    #[test]
+    fn day10_1() {
+        assert_eq_ansval!(339411, days::day10::day10_1("day10"));
+    }
+    #[test]
+    fn day10_2() {
+        assert_eq_ansval!(2289754624_usize, days::day10::day10_2("day10"));
+    }
+    #[test]
+    fn day11_1() {
+        assert_eq_ansval!(1741, days::day11::day11_1("day11"));
+    }
+    #[test]
+    fn day11_2() {
+        assert_eq_ansval!(440, days::day11::day11_2("day11"));
+    }
+    #[test]
+    fn day12_1() {
+        assert_eq_ansval!(4304, days::day12::day12_1("day12"));
+    }
+    #[test]
+    fn day12_2() {
+        assert_eq_ansval!(118242, days::day12::day12_2("day12"));
+    }
+    #[test]
+    fn day13_1() {
+        assert_eq_ansval!(607, days::day13::day13_1("day13"));
+    }
+    #[test]
+    fn day13_2() {
+        assert_eq_ansval!(".##..###..####.#....###..####.####.#....\n#..#.#..#....#.#....#..#.#.......#.#....\n#....#..#...#..#....#..#.###....#..#....\n#....###...#...#....###..#.....#...#....\n#..#.#....#....#....#....#....#....#....\n.##..#....####.####.#....#....####.####.\n", days::day13::day13_2("day13") );
+    }
+    #[test]
+    fn day14_1() {
+        assert_eq_ansval!(3259, days::day14::day14_1("day14"));
+    }
+    #[test]
+    fn day14_2() {
+        assert_eq_ansval!(3459174981021_usize, days::day14::day14_2("day14"));
+    }
+    #[test]
+    fn day15_1() {
+        assert_eq_ansval!(748, days::day15::day15_1("day15"));
+    }
+    #[test]
+    fn day15_2() {
+        assert_eq_ansval!(3045, days::day15::day15_2("day15"));
+    }
+    #[test]
+    fn day16_1() {
+        assert_eq_ansval!(1012, days::day16::day16_1("day16"));
+    }
+    #[test]
+    fn day16_2() {
+        assert_eq_ansval!(2223947372407_usize, days::day16::day16_2("day16"));
+    }
+    #[test]
+    #[ignore]
+    fn day17_1() {
+        assert_eq_ansval!(4656, days::day17::day17_1("day17"));
+    }
+    #[test]
+    #[ignore]
+    fn day17_2() {
+        assert_eq_ansval!(1908, days::day17::day17_2("day17"));
+    }
+    #[test]
+    fn day18_1() {
+        assert_eq_ansval!(3699, days::day18::day18_1("day18"));
+    }
+    #[test]
+    fn day18_2() {
+        assert_eq_ansval!(4735, days::day18::day18_2("day18"));
+    }
+    #[test]
+    fn day19_1() {
+        assert_eq_ansval!((), days::day19::day19_1("day19"));
+    }
+    #[test]
+    fn day19_2() {
+        assert_eq_ansval!((), days::day19::day19_2("day19"));
+    }
+    #[test]
+    fn day20_1() {
+        assert_eq_ansval!((), days::day20::day20_1("day20"));
+    }
+    #[test]
+    fn day20_2() {
+        assert_eq_ansval!((), days::day20::day20_2("day20"));
+    }
+    #[test]
+    fn day21_1() {
+        assert_eq_ansval!((), days::day21::day21_1("day21"));
+    }
+    #[test]
+    fn day21_2() {
+        assert_eq_ansval!((), days::day21::day21_2("day21"));
+    }
+    #[test]
+    fn day22_1() {
+        assert_eq_ansval!((), days::day22::day22_1("day22"));
+    }
+    #[test]
+    fn day22_2() {
+        assert_eq_ansval!((), days::day22::day22_2("day22"));
+    }
+    #[test]
+    fn day23_1() {
+        assert_eq_ansval!((), days::day23::day23_1("day23"));
+    }
+    #[test]
+    fn day23_2() {
+        assert_eq_ansval!((), days::day23::day23_2("day23"));
+    }
+    #[test]
+    fn day24_1() {
+        assert_eq_ansval!((), days::day24::day24_1("day24"));
+    }
+    #[test]
+    fn day24_2() {
+        assert_eq_ansval!((), days::day24::day24_2("day24"));
+    }
+    #[test]
+    fn day25_1() {
+        assert_eq_ansval!((), days::day25::day25_1("day25"));
+    }
+    #[test]
+    fn day25_2() {
+        assert_eq_ansval!((), days::day25::day25_2("day25"));
+    }
 }
