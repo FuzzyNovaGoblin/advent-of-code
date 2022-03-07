f0f1df218e27cf59de9086c02f63db041e782680
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2022-03-07 12:55:08 -0500

    day 20 part 1

--- 2021/aofc_2021/src/days/day20.rs
+++ 2021/aofc_2021/src/days/day20.rs
@@ -1,18 +1,187 @@
-use std::fs;
+use crate::point_map::CordPoint;
+use std::{
+    collections::HashMap,
+    default::default,
+    fs,
+    ops::{Index, IndexMut},
+};
+
+#[derive(Debug)]
+struct PointHashMap {
+    map: HashMap<CordPoint, char>,
+    default_point: char,
+}
+
+impl Default for PointHashMap {
+    fn default() -> Self {
+        Self {
+            map: Default::default(),
+            default_point: '.',
+        }
+    }
+}
+
+impl Index<CordPoint> for PointHashMap {
+    type Output = char;
+
+    fn index(&self, index: CordPoint) -> &Self::Output {
+        if self.map.contains_key(&index) {
+            self.map.get(&index).unwrap()
+        } else {
+            &self.default_point
+        }
+    }
+}
+impl IndexMut<CordPoint> for PointHashMap {
+    fn index_mut(&mut self, index: CordPoint) -> &mut Self::Output {
+        if !self.map.contains_key(&index) {
+            self.map.insert(index.clone(), '.');
+        }
+        self.map.get_mut(&index).unwrap()
+    }
+}
+
+impl PointHashMap {
+    fn get_surounding_points(&self, point: &CordPoint) -> Vec<char> {
+        let mut ret_val = Vec::new();
+        for y in point.y - 1..=point.y + 1 {
+            for x in point.x - 1..=point.x + 1 {
+                ret_val.push(self[CordPoint::new(x, y)]);
+            }
+        }
+        ret_val
+    }
+    fn get_surounding_points_num(
+        &self,
+        /*         save_map: HashMap<Vec<char>, usize>,
+         */ point: &CordPoint,
+    ) -> usize {
+        let chars = self.get_surounding_points(point);
+
+        // loop {
+        //     if save_map.contains_key(&chars) {
+        //         break;
+        //     }
+
+        let mut ret_val = 0;
+        for c in chars {
+            if c == '#' {
+                ret_val += 1;
+            }
+            ret_val <<= 1;
+        }
+        ret_val >>= 1;
+
+        // break;
+        // }
+
+        ret_val
+    }
+
+    fn print_map(&self) {
+        let (min, max) = self.get_dimentions();
+        for y in min.y..=max.y {
+            for x in min.x..=max.x {
+                print!("{}", self[CordPoint::new(x, y)])
+            }
+            println!();
+        }
+    }
+    fn get_dimentions(&self) -> (CordPoint, CordPoint) {
+        let (mut max_x, mut min_x, mut max_y, mut min_y) = (0, 0, 0, 0);
+        for key in self.map.keys() {
+            min_x = min_x.min(key.x);
+            max_x = max_x.max(key.x);
+            min_y = min_y.min(key.y);
+            max_y = max_y.max(key.y);
+        }
+        (CordPoint::new(min_x, min_y), CordPoint::new(max_x, max_y))
+    }
+}
 
 pub fn day20_1(file_name: &str) -> impl crate::AnsType {
-let input_file = format!("{}/aofc_2021/input/{}", env!("ADVENT_OF_CODE_2021"), file_name);
-    let _data = fs::read_to_string(input_file);
-	todo!()
+    let input_file = format!(
+        "{}/aofc_2021/input/{}",
+        env!("ADVENT_OF_CODE_2021"),
+        file_name
+    );
+    let data = fs::read_to_string(input_file)
+        .unwrap()
+        .split("\n\n")
+        .map(str::to_owned)
+        .collect::<Vec<String>>();
+    let image_enhancement_algorithm = data[0].chars().collect::<Vec<char>>();
+    let mut input_image: PointHashMap = PointHashMap::default();
+
+    for (y, line) in data[1].split('\n').enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            input_image[CordPoint::new(x as i128, y as i128)] = c;
+        }
+    }
+    for _ in 0..2 {
+        let (min, max) = input_image.get_dimentions();
+        let mut new_image: PointHashMap = default();
+        for y in min.y - 1..=max.y + 1 {
+            for x in min.x - 1..=max.x + 1 {
+                new_image[CordPoint::new(x, y)] = image_enhancement_algorithm
+                    [input_image.get_surounding_points_num(&CordPoint::new(x, y))];
+            }
+        }
+        input_image = new_image;
+        input_image.default_point = if input_image.default_point == '.' {
+            image_enhancement_algorithm[0]
+        } else {
+            image_enhancement_algorithm[1]
+        }
+    }
+
+    input_image.map.into_iter().fold(
+        0,
+        |f_val, (_, n_val)| if n_val == '#' { f_val + 1 } else { f_val },
+    )
 }
 
 pub fn day20_2(file_name: &str) -> impl crate::AnsType {
-let input_file = format!("{}/aofc_2021/input/{}", env!("ADVENT_OF_CODE_2021"), file_name);
-    let _data = fs::read_to_string(input_file);
-	todo!()
+    let input_file = format!(
+        "{}/aofc_2021/input/{}",
+        env!("ADVENT_OF_CODE_2021"),
+        file_name
+    );
+    let data = fs::read_to_string(input_file)
+        .unwrap()
+        .split("\n\n")
+        .map(str::to_owned)
+        .collect::<Vec<String>>();
+    let image_enhancement_algorithm = data[0].chars().collect::<Vec<char>>();
+    let mut input_image: PointHashMap = PointHashMap::default();
+
+    for (y, line) in data[1].split('\n').enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            input_image[CordPoint::new(x as i128, y as i128)] = c;
+        }
+    }
+    for _ in 0..50 {
+        let (min, max) = input_image.get_dimentions();
+        let mut new_image: PointHashMap = default();
+        for y in min.y - 1..=max.y + 1 {
+            for x in min.x - 1..=max.x + 1 {
+                new_image[CordPoint::new(x, y)] = image_enhancement_algorithm
+                    [input_image.get_surounding_points_num(&CordPoint::new(x, y))];
+            }
+        }
+        input_image = new_image;
+        input_image.default_point = if input_image.default_point == '.' {
+            image_enhancement_algorithm[0]
+        } else {
+            image_enhancement_algorithm[1]
+        }
+    }
+
+    input_image.map.into_iter().fold(
+        0,
+        |f_val, (_, n_val)| if n_val == '#' { f_val + 1 } else { f_val },
+    )
 }
-
-
 
 #[cfg(test)]
 mod test {
@@ -20,9 +189,8 @@ mod test {
     use crate::assert_eq_ansval;
 
     #[test]
-    #[ignore]
     fn t1() {
-        assert_eq_ansval!((), day20_1("test"));
+        assert_eq_ansval!(35, day20_1("test"));
     }
     #[test]
     #[ignore]
--- 2021/aofc_2021/src/point_map/mod.rs
+++ 2021/aofc_2021/src/point_map/mod.rs
@@ -1,5 +1,323 @@
 #![allow(dead_code)]
 #![allow(clippy::unusual_byte_groupings)]
-include!("./point_map.rs");
 pub mod iterator;
 pub use iterator::DimentionIter;
+use std::{
+    fmt::{Debug, Display},
+    ops,
+};
+
+#[derive(Default, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct CordPoint {
+    pub x: i128,
+    pub y: i128,
+}
+
+impl CordPoint {
+    pub fn new(x: i128, y: i128) -> Self { Self { x, y } }
+}
+
+impl From<CordPointTuple> for CordPoint {
+    fn from(other: (usize, usize)) -> Self {
+        Self {
+            x: other.0 as i128,
+            y: other.1 as i128,
+        }
+    }
+}
+
+pub type CordPointTuple = (usize, usize);
+
+#[derive(Default, Clone)]
+pub struct PointMap<T> {
+    points: Vec<Vec<T>>,
+}
+
+impl<T> PointMap<T>
+where
+    T: Display + Default,
+{
+    pub fn from(points: Vec<Vec<T>>) -> Self {
+        Self { points }
+    }
+    pub fn get_boardering_points_with_center(&self, point: CordPointTuple) -> Vec<CordPointTuple> {
+        self.get_boardering_points(point, 0b_111_111_111)
+    }
+
+    pub fn get_boardering_points(
+        &self,
+        (x, y): CordPointTuple,
+        include_mask: u16,
+    ) -> Vec<CordPointTuple> {
+        let mut ret_points = vec![];
+        // first layer
+        if y > 0 {
+            if (0b_100_000_000 & include_mask) > 0 && x > 0 {
+                ret_points.push((x - 1, y - 1));
+            }
+            if (0b_010_000_000 & include_mask) > 0 {
+                ret_points.push((x, y - 1));
+            }
+
+            if (0b_001_000_000 & include_mask) > 0 && x < self.points.len() - 1 {
+                ret_points.push((x + 1, y - 1));
+            }
+        }
+
+        // second layer
+
+        if (0b_000_100_000 & include_mask) > 0 && x > 0 {
+            ret_points.push((x - 1, y));
+        }
+
+        if (0b_000_010_000 & include_mask) > 0 {
+            ret_points.push((x, y));
+        }
+
+        if (0b_000_001_000 & include_mask) > 0 && x < self.points.len() - 1 {
+            ret_points.push((x + 1, y));
+        }
+
+        // third layer
+        if y < self.points[0].len() - 1 {
+            if (0b_000_000_100 & include_mask) > 0 && x > 0 {
+                ret_points.push((x - 1, y + 1));
+            }
+            if (0b_000_000_010 & include_mask) > 0 {
+                ret_points.push((x, y + 1));
+            }
+
+            if (0b_000_000_001 & include_mask) > 0 && x < self.points.len() - 1 {
+                ret_points.push((x + 1, y + 1));
+            }
+        }
+
+        ret_points
+    }
+
+    pub fn print_around_point(&self, x: usize, y: usize) {
+        // first layer
+        if y > 0 {
+            if x > 0 {
+                print!("{}", self[(x - 1, y - 1)]);
+            } else {
+                print!("*");
+            }
+            print!("{}", self[(x, y - 1)]);
+            if x < self.points.len() - 1 {
+                print!("{}", self[(x + 1, y - 1)]);
+            } else {
+                print!("*");
+            }
+        } else {
+            print!("***");
+        }
+
+        // second layer
+        println!();
+        if x > 0 {
+            print!("{}", self[(x - 1, y)]);
+        } else {
+            print!("*");
+        }
+        print!("{}", self[(x, y)]);
+        if x < self.points.len() - 1 {
+            print!("{}", self[(x + 1, y)]);
+        } else {
+            print!("*");
+        }
+        println!();
+
+        // third layer
+        if y < self.points[0].len() - 1 {
+            if x > 0 {
+                print!("{}", self[(x - 1, y + 1)]);
+            } else {
+                print!("*");
+            }
+            print!("{}", self[(x, y + 1)]);
+            if x < self.points.len() - 1 {
+                print!("{}", self[(x + 1, y + 1)]);
+            } else {
+                print!("*");
+            }
+        } else {
+            print!("***");
+        }
+        println!();
+    }
+
+    pub fn len(&self) -> usize {
+        let d = self.get_dimentions();
+        d.0 * d.1
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+}
+
+impl<T> PointMap<T> {
+    pub fn get_dimentions(&self) -> CordPointTuple {
+        (
+            self.points.len(),
+            self.points.get(0).unwrap_or(&Vec::new()).len(),
+        )
+    }
+}
+
+auto trait NotString {}
+
+impl !NotString for String {}
+
+impl<T> From<PointMap<T>> for PointMap<String>
+where
+    T: Display + NotString,
+{
+    fn from(p: PointMap<T>) -> Self {
+        PointMap::from(
+            p.points
+                .into_iter()
+                .map(|v| v.into_iter().map(|item| format!("{}", item)).collect())
+                .collect(),
+        )
+    }
+}
+
+impl<T> From<Vec<Vec<T>>> for PointMap<T>
+where
+    T: Default + Display + Ord,
+{
+    fn from(from_data: Vec<Vec<T>>) -> Self {
+        let mut ret_val = PointMap::default();
+        for (i, d) in from_data.into_iter().enumerate() {
+            for (j, d) in d.into_iter().enumerate() {
+                ret_val[(i, j)] = d;
+            }
+        }
+        ret_val
+    }
+}
+
+impl<T> Debug for PointMap<T>
+where
+    T: Display + Default,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "PointMap {{\n\tdimentions: {:?},\n\tmap:\n{}\n}}",
+            self.get_dimentions(),
+            format!("{}", self)
+                .split('\n')
+                .map(|s| format!("\t     {}\n", s))
+                .collect::<String>()
+        )
+    }
+}
+
+impl<T> Display for PointMap<T>
+where
+    T: Display + Default,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut max_size = 0;
+        for point in DimentionIter::new(self.get_dimentions()) {
+            max_size = std::cmp::max(format!("{}", self[point]).len(), max_size);
+        }
+
+        let write_h_line = |f: &mut std::fmt::Formatter<'_>| -> std::fmt::Result {
+            write!(f, "{}", "*".repeat(self.points.len() * (max_size + 1) + 4))
+        };
+
+        write_h_line(f)?;
+        writeln!(f)?;
+        for y in 0..self.points[0].len() {
+            write!(f, "**")?;
+            for x in 0..self.points.len() {
+                write!(f, "{:max_size$} ", self.points[x][y], max_size = max_size)?;
+            }
+            writeln!(f, "**")?;
+        }
+        write_h_line(f)?;
+
+        Ok(())
+    }
+}
+
+impl<T> ops::Index<CordPointTuple> for PointMap<T> {
+    type Output = T;
+
+    fn index(&self, (x, y): CordPointTuple) -> &Self::Output {
+        &self.points[x][y]
+    }
+}
+
+impl<T> ops::IndexMut<CordPointTuple> for PointMap<T>
+where
+    T: Display + Default + PartialOrd,
+{
+    fn index_mut(&mut self, (x, y): CordPointTuple) -> &mut Self::Output {
+        let (_, height) = self.get_dimentions();
+        while x >= self.points.len() {
+            self.points.push(Default::default());
+            self.points.last_mut().into_iter().for_each(|column| {
+                while column.len() < height {
+                    column.push(Default::default());
+                }
+            });
+        }
+        while y >= self.points[x].len() {
+            self.points
+                .iter_mut()
+                .for_each(|column| column.push(Default::default()));
+        }
+
+        &mut self.points[x][y]
+    }
+}
+
+// impl<T> ops::Index<CordPoint> for PointMap<T>
+// where
+//     T: Display + Default + PartialOrd,
+// {
+//     type Output = T;
+
+//     fn index(&self, cord: CordPoint) -> &Self::Output {
+//         if cord.x >= 0 && cord.y >= 0 {
+//             self.index((cord.x as usize, cord.y as usize))
+//         } else {
+//             panic!("cant index unmutable with negetive numbers");
+//         }
+//     }
+// }
+
+// impl<T> ops::IndexMut<CordPoint> for PointMap<T>
+// where
+//     T: Display + Default + PartialOrd,
+// {
+//     fn index_mut(&mut self, cord: CordPoint) -> &mut Self::Output {
+//         if cord.x >= 0 && cord.y >= 0 {
+//             return self.index_mut((cord.x as usize, cord.y as usize));
+//         }
+
+//         let (_, height) = self.get_dimentions();
+
+//         while cord.x < 0 {
+//             self.points.insert(0, Default::default());
+//             self.points.first_mut().into_iter().for_each(|column| {
+//                 while column.len() < height {
+//                     column.push(Default::default());
+//                 }
+//             });
+//             cord.x += 1;
+//         }
+//         while cord.y < 0 {
+//             for column in self.points.iter_mut() {
+//                 column.insert(0, Default::default());
+//                 cord.y += 1;
+//             }
+//         }
+//         self.index_mut((0_usize, 0_usize))
+//     }
+// }
