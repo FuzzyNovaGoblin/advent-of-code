632de06d15208a0952a317ead684af552e943840
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2022-12-04 08:34:10 -0500

    day4

--- 2022/aofc_2022/src/days/day2.rs
+++ 2022/aofc_2022/src/days/day2.rs
@@ -19,49 +19,49 @@ impl Outcome {
 }
 
 #[derive(Debug, Clone, Copy)]
-enum RPS {
+enum Rps {
     Rock,
     Paper,
     Scissors,
 }
-impl RPS {
-    fn from_letter(letter: char) -> RPS {
+impl Rps {
+    fn from_letter(letter: char) -> Rps {
         match letter {
-            'A' => RPS::Rock,
-            'B' => RPS::Paper,
-            'C' => RPS::Scissors,
-            'X' => RPS::Rock,
-            'Y' => RPS::Paper,
-            'Z' => RPS::Scissors,
+            'A' => Rps::Rock,
+            'B' => Rps::Paper,
+            'C' => Rps::Scissors,
+            'X' => Rps::Rock,
+            'Y' => Rps::Paper,
+            'Z' => Rps::Scissors,
             c => unreachable!("{}", c),
         }
     }
 
-    fn corisponding_outcome(&self, desired: Outcome) -> RPS {
+    fn corisponding_outcome(&self, desired: Outcome) -> Rps {
         match (self, desired) {
-            (RPS::Rock, Outcome::Lose) => RPS::Scissors,
-            (RPS::Rock, Outcome::Draw) => RPS::Rock,
-            (RPS::Rock, Outcome::Win) => RPS::Paper,
-            (RPS::Paper, Outcome::Lose) => RPS::Rock,
-            (RPS::Paper, Outcome::Draw) => RPS::Paper,
-            (RPS::Paper, Outcome::Win) => RPS::Scissors,
-            (RPS::Scissors, Outcome::Lose) => RPS::Paper,
-            (RPS::Scissors, Outcome::Draw) => RPS::Scissors,
-            (RPS::Scissors, Outcome::Win) => RPS::Rock,
+            (Rps::Rock, Outcome::Lose) => Rps::Scissors,
+            (Rps::Rock, Outcome::Draw) => Rps::Rock,
+            (Rps::Rock, Outcome::Win) => Rps::Paper,
+            (Rps::Paper, Outcome::Lose) => Rps::Rock,
+            (Rps::Paper, Outcome::Draw) => Rps::Paper,
+            (Rps::Paper, Outcome::Win) => Rps::Scissors,
+            (Rps::Scissors, Outcome::Lose) => Rps::Paper,
+            (Rps::Scissors, Outcome::Draw) => Rps::Scissors,
+            (Rps::Scissors, Outcome::Win) => Rps::Rock,
         }
     }
 
-    fn fight(&self, other: &RPS) -> u32 {
+    fn fight(&self, other: &Rps) -> u32 {
         match (self, other) {
-            (RPS::Rock, RPS::Rock) => 1 + 3,
-            (RPS::Rock, RPS::Paper) => 1,
-            (RPS::Rock, RPS::Scissors) => 1 + 6,
-            (RPS::Paper, RPS::Rock) => 2 + 6,
-            (RPS::Paper, RPS::Paper) => 2 + 3,
-            (RPS::Paper, RPS::Scissors) => 2,
-            (RPS::Scissors, RPS::Rock) => 3,
-            (RPS::Scissors, RPS::Paper) => 3 + 6,
-            (RPS::Scissors, RPS::Scissors) => 3 + 3,
+            (Rps::Rock, Rps::Rock) => 1 + 3,
+            (Rps::Rock, Rps::Paper) => 1,
+            (Rps::Rock, Rps::Scissors) => 1 + 6,
+            (Rps::Paper, Rps::Rock) => 2 + 6,
+            (Rps::Paper, Rps::Paper) => 2 + 3,
+            (Rps::Paper, Rps::Scissors) => 2,
+            (Rps::Scissors, Rps::Rock) => 3,
+            (Rps::Scissors, Rps::Paper) => 3 + 6,
+            (Rps::Scissors, Rps::Scissors) => 3 + 3,
         }
     }
 }
@@ -78,8 +78,8 @@ pub fn day2_1(file_name: &str) -> impl crate::AnsType {
         .map(|line| {
             let mut chars = line.chars();
             (
-                RPS::from_letter(chars.next().unwrap()),
-                RPS::from_letter(chars.nth(1).unwrap()),
+                Rps::from_letter(chars.next().unwrap()),
+                Rps::from_letter(chars.nth(1).unwrap()),
             )
         })
         .fold(0, |sum, (other, you)| you.fight(&other) + sum)
@@ -97,7 +97,7 @@ pub fn day2_2(file_name: &str) -> impl crate::AnsType {
         .map(|line| {
             let mut chars = line.chars();
             (
-                RPS::from_letter(chars.next().unwrap()),
+                Rps::from_letter(chars.next().unwrap()),
                 Outcome::from_letter(chars.nth(1).unwrap()),
             )
         })
--- 2022/aofc_2022/src/days/day4.rs
+++ 2022/aofc_2022/src/days/day4.rs
@@ -1,18 +1,62 @@
 use std::fs;
 
 pub fn day4_1(file_name: &str) -> impl crate::AnsType {
-let input_file = format!("{}/aofc_2022/input/{}", env!("ADVENT_OF_CODE_2022"), file_name);
-    let _data = fs::read_to_string(input_file);
-	todo!()
+    let input_file = format!(
+        "{}/aofc_2022/input/{}",
+        env!("ADVENT_OF_CODE_2022"),
+        file_name
+    );
+    let data = fs::read_to_string(input_file);
+    data.unwrap()
+        .lines()
+        .map(|line| {
+            let mut pair = line.split(',');
+            (pair.next().unwrap(), pair.next().unwrap())
+        })
+        .map(|(rng1, rng2)| {
+            let mut rng1 = rng1.split('-').map(|s| s.parse::<u32>());
+            let mut rng2 = rng2.split('-').map(|s| s.parse::<u32>());
+            (
+                (rng1.next().unwrap().unwrap()..=rng1.next().unwrap().unwrap()),
+                (rng2.next().unwrap().unwrap()..=rng2.next().unwrap().unwrap()),
+            )
+        })
+        .filter(|(rng1, rng2)| {
+            rng2.contains(rng1.start()) && rng2.contains(rng1.end())
+                || rng1.contains(rng2.start()) && rng1.contains(rng2.end())
+        })
+        .count()
 }
 
 pub fn day4_2(file_name: &str) -> impl crate::AnsType {
-let input_file = format!("{}/aofc_2022/input/{}", env!("ADVENT_OF_CODE_2022"), file_name);
-    let _data = fs::read_to_string(input_file);
-	todo!()
+    let input_file = format!(
+        "{}/aofc_2022/input/{}",
+        env!("ADVENT_OF_CODE_2022"),
+        file_name
+    );
+    let data = fs::read_to_string(input_file);
+    data.unwrap()
+        .lines()
+        .map(|line| {
+            let mut pair = line.split(',');
+            (pair.next().unwrap(), pair.next().unwrap())
+        })
+        .map(|(rng1, rng2)| {
+            let mut rng1 = rng1.split('-').map(|s| s.parse::<u32>());
+            let mut rng2 = rng2.split('-').map(|s| s.parse::<u32>());
+            (
+                (rng1.next().unwrap().unwrap()..=rng1.next().unwrap().unwrap()),
+                (rng2.next().unwrap().unwrap()..=rng2.next().unwrap().unwrap()),
+            )
+        })
+        .filter(|(rng1, rng2)| {
+            rng2.contains(rng1.start())
+                || rng2.contains(rng1.end())
+                || rng1.contains(rng2.start())
+                || rng1.contains(rng2.end())
+        })
+        .count()
 }
-
-
 
 #[cfg(test)]
 mod test {
@@ -20,9 +64,9 @@ mod test {
     use crate::assert_eq_ansval;
 
     #[test]
-    #[ignore]
+
     fn t1() {
-        assert_eq_ansval!((), day4_1("test"));
+        assert_eq_ansval!(2, day4_1("test"));
     }
     #[test]
     #[ignore]
